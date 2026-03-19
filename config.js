5b6303e400dc122d3de121095463e53568492b45
BotsByLarry <advaitpathak27@proton.me>
2026-03-19 21:20:37 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,33 @@
+name: Inject Secrets
+
+on:
+  push:
+    branches: [main]
+  workflow_dispatch:
+
+jobs:
+  inject:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      
+      - name: Create config with secrets
+        run: |
+          cat > config.js << 'EOF'
+          const WEBHOOKS = {
+    "announcements": "https://discord.com/api/webhooks/1483854676934918225/_2mmWLi1PuBxV58ikEXo1aupaGCWUPyYijmQqhROIvgwD8UW54xW3UTNW4cCwBDHHmH-",
+    "mod-announcements": "https://discord.com/api/webhooks/1483871790164869140/-QJ7GO4J6SsPggzmBWekYmLfRdfmA5FiWUnsmIhOdaB59OEMK6Lpn853zKGTZGQZvkl0",
+    "info": "https://discord.com/api/webhooks/1483855941349937242/gUrGal0FXsbGBggpcOuUJs1RUB9xsGtPDMqkqRD_7WzAW3eYEdHfDtDRyGvGh9ILuXMC",
+    "paid-roles": "https://discord.com/api/webhooks/1483854664150552666/niZOU25_tQZeFUBwdQCL9mtZJ9q29UlajbVGjbokzr1QuHbvmHGPITLW1bU6EdWNRzfT",
+    "rules": "https://discord.com/api/webhooks/1483857076152303667/36nAl0cUhsM5dZc0B2oqPTcRFW2tuiYFq8cYqNfp7qzrn77nyKhkwYxVek8j0oBPcJrF",
+    "request-middleman": "https://discord.com/api/webhooks/1483857172382355667/MiSm48_r0l9U1BU8HVk3XBRGHlBxorVSJOhMCCEJxBc7aiXB58atEukdKGzEUg70ldHx",
+          };
+          EOF
+      
+      - name: Deploy
+        run: |
+          git config --local user.email "action@github.com"
+          git config --local user.name "GitHub Action"
+          git add config.js
+          git commit -m "Auto-inject secrets"
+          git push
